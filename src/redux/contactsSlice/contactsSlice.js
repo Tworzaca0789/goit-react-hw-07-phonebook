a2dac6a2bf142d5c1d6b5d323b8f57bd780b6add
Joanna Matwiej <126902190+Tworzaca0789@users.noreply.github.com>
2023-12-05 22:33:37 +0100

    Add consts and services files

--- src/redux/contactsSlice/contactsSlice.js
+++ src/redux/contactsSlice/contactsSlice.js
@@ -5,16 +5,16 @@ export const ContactsSlice = createSlice({
   initialState: {
     contacts: [], // contactsInitialState
   },
-  reducers: {
-    addContact(state, action) {
-      state.contacts.push(action.payload);
-    },
-    deleteContact(state, action) {
-      state.contacts = state.contacts.filter(
-        contact => contact.id !== action.payload
-      );
-    },
-  },
+  // reducers: {
+  //   addContact(state, action) {
+  //     state.contacts.push(action.payload);
+  //   },
+  //   deleteContact(state, action) {
+  //     state.contacts = state.contacts.filter(
+  //       contact => contact.id !== action.payload
+  //     );
+  //   },
+  // },
 });
 
 export const { addContact, deleteContact } = ContactsSlice.actions;
